e917028b0176922554b9aff0dd995dfe6cd6c5e2
songluyi <slysly759@gmail.com>
2016-09-23 09:47:45 +0800

    将pytesser修改为3.x版本

---
+++ code.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# 2016/9/22 16:18
+"""
+-------------------------------------------------------------------------------
+Function:
+Version:    1.0
+Author:     SLY
+Contact:    slysly759@gmail.com 
+ 
+-------------------------------------------------------------------------------
+"""
+
+from pytesser3 import image_to_string
+from PIL import Image
+import requests
+def get_code():
+        url = 'http://www.rongtudai.com/validimg.html'
+        f=requests.get(url)
+        print(f)
+        with open("code.jpg", "wb") as code:
+            code.write(f.content)
+        img = Image.open('code.jpg')
+        img = img.convert("RGBA")
+        pixdata = img.load()
+        for y in range(img.size[1]):
+             for x in range(img.size[0]):
+                if pixdata[x, y][0] < 90:
+                    pixdata[x, y] = (0, 0, 0, 255)
+        for y in range(img.size[1]):
+            for x in range(img.size[0]):
+                if pixdata[x, y][1] < 136:
+                    pixdata[x, y] = (0, 0, 0, 255)
+        for y in range(img.size[1]):
+            for x in range(img.size[0]):
+                if pixdata[x, y][2] > 0:
+                    pixdata[x, y] = (255, 255, 255, 255)
+        img.save('newcode.jpg')
+        img = Image.open('newcode.jpg')
+        vcode =image_to_string(img)
+        return vcode
+print(get_code())
---
+++ readme.md
@@ -0,0 +1,18 @@
+#pytesser3 使用说明
+
+##需要环境
+1. Python3.x以上
+
+2. 需要安装PIL以及tesseract-ocr引擎。[点我下载tesseract-ocr引擎](http://101.96.10.43/internode.dl.sourceforge.net/project/tesseract-ocr-alt/tesseract-ocr-setup-3.02.02.exe)
+
+##如何使用
+
+1. 下载pytesser3 解压后，放置在你的lib/site-packages 目录下
+
+2. 修改__init__.py里面第十二行tesseract_exe_name为你tesseract-ocr安装路径，我填写的就是tesseract-ocr默认安装的，如果
+你也是默认安装路径，那么请忽略本步骤，即可直接运行
+
+##如何测试轮子是否好用
+
+1. 你可以copy你下载的code.py 运行一下即可检验轮子是否成功。
+
